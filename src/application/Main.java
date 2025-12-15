915bd5cdd7bedfc43e16dcce62abff853f539894
Arthur Dall Agnol Pinheiro <arthurdallagnol0702@gmail.com>
2025-12-15 11:23:56 -0300

    findById() implemented

---
+++ src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import dao.DaoFactory;
+import dao.SellerDao;
+import entities.Seller;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
+    }
+}
